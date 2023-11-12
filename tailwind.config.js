700fb9105216968cb492ab6755f12ade9a0584f2
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-11-12 20:56:53 +0800

    Update tailwind css config with color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				heading: ['"Skavi 27px"', '"Roboto Slab"', 'serif'],
+				main: ['Inter', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+				mono: ['"Source Code Pro"', 'monospace']
+			},
+			colors: {
+				primary: {
+					light: '#07004D',
+					DEFAULT: '#07004D'
+				},
+				secondary: {
+					light: '#2D82B7',
+					DEFAULT: '#2D82B7'
+				},
+				accent: {
+					light: '#42E2B8',
+					DEFAULT: '#42E2B8'
+				},
+				danger: {
+					light: '#EB8A90',
+					DEFAULT: '#EB8A90'
+				},
+				white: '#FFFFFF',
+				gray: colors.neutral
+			}
+		}
 	},
 	plugins: []
 };
