a746d6250e6ad865bf6243addb8d4024a559b340
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-11-12 20:59:46 +0800

    Add responsive header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,54 +1,21 @@
 <script>
 	import Header from './Header.svelte';
+	import Contact from './Contact.svelte';
 	import './styles.css';
 	import '../app.css';
 </script>
 
-<div class="app">
+<div class="flex flex-col min-h-max">
 	<Header />
 
-	<main>
+	<main class="md:container md:mx-auto">
 		<slot />
 	</main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	<Contact />
+
+	<footer class="flex flex-row justify-center items-center">
+		<span class="pr-5">Built with <a href="https://kit.svelte.dev">SvelteKit</a></span>
+		<span class="pr-5">© {new Date().getFullYear()} Wong Zhao Wu</span>
 	</footer>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,129 +1,58 @@
 <script>
-	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+	import svgMenu from '$lib/images/menu-alt-1-svgrepo-com.svg';
+	import svgClose from '$lib/images/cross-svgrepo-com.svg';
+	export let navs = ['about', 'personality', 'skills', 'experience', 'projects'];
+
+	let menu_visible = false;
 </script>
 
-<header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
-
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
+<header class="font-heading text-primary">
+	<nav class="px-4 py-6 flex justify-between items-center w-full">
+		<ul class="hidden md:flex ml-auto justify-start">
+			{#each navs as nav}
+				<li class="px-3">
+					<a class="hover:text-secondary focus:text-secondary focus:outline-none" href="#{nav}"
+						>{nav.charAt(0).toUpperCase() + nav.slice(1)}</a
+					>
+				</li>
+			{/each}
 		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
+
+		<!-- Menu Navigation on Mobile -->
+		<button
+			class="ml-auto px-2 h-10 w-10 z-30 md:hidden rounded-full hover:bg-secondary focus:bg-secondary"
+			on:click={() => {
+				menu_visible = !menu_visible;
+			}}
+		>
+			{#if !menu_visible}
+				<img class="text-primary object-contain w-10 h-10 -scale-x-100" src={svgMenu} alt="menu" />
+			{:else}
+				<img
+					class="text-primary object-contain w-10 h-10 -scale-x-100"
+					src={svgClose}
+					alt="close"
+				/>
+			{/if}
+		</button>
+		<div
+			class="fixed inset-0 md:hidden bg-black bg-opacity-50 z-20 opacity-100 {menu_visible
+				? 'visible'
+				: 'invisible'}"
+		></div>
+		<ul
+			class="fixed inset-y-0 md:hidden right-0 bg-white flex flex-col justify-center transform transition-all duration-300 z-20 overflow-hidden translate-x-0 shadow-lg {menu_visible
+				? 'visible w-56'
+				: 'invisible w-0'}"
+		>
+			{#each navs as nav}
+				<li class="px-3">
+					<a
+						class="mb-8 px-4 py-2 text-center block hover:text-secondary focus:text-secondary focus:outline-none"
+						href="#{nav}">{nav.charAt(0).toUpperCase() + nav.slice(1)}</a
+					>
+				</li>
+			{/each}
+		</ul>
 	</nav>
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
 </header>
-
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
-</style>
