56f601357bb28e9364879abe11c10f9fd0c23fff
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-11-13 13:36:18 +0800

    Add typewriter effect to name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Typewriter from 'svelte-typewriter';
 	import profile from '$lib/profile.json';
 </script>
 
@@ -8,13 +9,14 @@
 	<title>{profile.contact.firstName} {profile.contact.lastName}</title>
 </svelte:head>
 
-<section class="px-6 md:px-0 flex items-center min-h-screen text-xl">
-	<main>
+<section class="px-6 md:px-0 flex items-center min-h-screen max-w-6xl text-xl font-main">
+	<main class="">
 		<h3 class="my-3 font-mono text-secondary">Hi, my name is</h3>
-		<h1 class="my-5 text-primary text-7xl text-left">
-			{profile.contact.firstName}
-			{profile.contact.lastName}
-		</h1>
+		<Typewriter mode="loopOnce" keepCursorOnFinish={true}>
+			{#each [`${profile.contact.firstName}`, `${profile.contact.ign}`, `${profile.contact.nickName} ${profile.contact.lastName}`, `${profile.contact.firstName} (${profile.contact.nickName}) ${profile.contact.lastName}`] as text}
+				<h1 class="my-5 text-7xl text-left font-semibold">{text}</h1>
+			{/each}
+		</Typewriter>
 		<h2>Aspiring AI Engineer</h2>
 	</main>
 </section>
