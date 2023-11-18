ee9f67fabf4d7a257e4f2f1c851d28d67f62e7ea
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-11-18 12:52:02 +0800

    Tweeks to hero section font size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -29,16 +29,16 @@
 
 <section id="hero" class="flex items-center min-h-screen max-w-6xl text-xl font-primary">
 	<header>
-		<h3 class="my-3 font-mono text-secondary text-xl">Hi, my name is</h3>
+		<h3 class="my-3 font-mono text-secondary text-lg md:text-xl">Hi, my name is</h3>
 		<Typewriter mode="loopOnce" keepCursorOnFinish={true} wordInterval={700}>
 			{#each [`${profile.profile.firstName}`, `${profile.profile.ign}`, `${profile.profile.nickName} ${profile.profile.lastName}`, `${profile.profile.nickName} ${profile.profile.lastName} ${profile.profile.firstName}`] as text}
-				<h1 class="my-3 text-6xl text-left">{text}</h1>
+				<h1 class="my-3 text-5xl md:text-6xl text-left">{text}</h1>
 			{/each}
 		</Typewriter>
-		<h1 class="my-5 text-4xl text-left font-extrabold font-heading text-secondary">
+		<h1 class="my-5 text-xl md:text-4xl text-left font-extrabold font-heading text-secondary">
 			NUSC w/ CS @ 2029
 		</h1>
-		<h3 class="my-2">
+		<h3 class="my-2 text-lg md:text-xl">
 			💡 Conscientiously Driven | 💻 Aspiring ML Engineer | 🏞️ Outdoor Adventurer
 		</h3>
 	</header>
