df4cc5a6ca88bfea5e5f0d17c920ab4904491546
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2023-11-27 22:27:42 +0800

    Modularized hero banner into Svelte Component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import { marked } from 'marked';
 	import moment from 'moment';
-	import Typewriter from 'svelte-typewriter';
-	import { onMount } from 'svelte';
 	import profile from '$lib/profile.json';
 	import profilePic from '$lib/images/profile-pic-cropped.jpg';
 
@@ -10,6 +8,9 @@
 	import icon_link from '$lib/images/link-linear.svg';
 	import icon_github from '$lib/images/github.svg';
 	import icon_linkedin from '$lib/images/linkedin.svg';
+
+	// Import Components
+	import Hero from './Hero.svelte';
 
 	// Change visble section
 	let visibleSectionIdx = 0;
@@ -28,25 +29,7 @@
 	>
 </svelte:head>
 
-<section id="hero" class="text-xl font-primary">
-	<header class="flex items-center min-h-screen max-w-6xl">
-		<div class="">
-			<h3 class="my-3 font-mono text-secondary text-lg md:text-xl">Hi, my name is</h3>
-			<Typewriter mode="loopOnce" keepCursorOnFinish={true} wordInterval={700}>
-				{#each [`${profile.profile.firstName}`, `${profile.profile.ign}`, `${profile.profile.nickName} ${profile.profile.lastName}`, `${profile.profile.nickName} ${profile.profile.lastName} ${profile.profile.firstName}`] as text}
-					<h1 class="my-3 text-5xl md:text-6xl text-left">{text}</h1>
-				{/each}
-			</Typewriter>
-			<h1 class="my-5 text-xl md:text-4xl text-left font-extrabold font-heading text-secondary">
-				NUSC w/ CS @ 2029
-			</h1>
-			<h3 class="my-2 text-lg md:text-xl">
-				💡 Conscientiously Driven | 💻 Aspiring ML Engineer | 🏞️ Outdoor Enthusiasts
-			</h3>
-		</div>
-	</header>
-	<div id="graph" class="absolute my-auto top-0 right-0 w-4/5 h-4/5 -z-10"></div>
-</section>
+<Hero {...profile.profile} />
 
 <section id="about" class="my-10">
 	<h1 class="my-5">About Me</h1>
---
+++ src/routes/Hero.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Typewriter from 'svelte-typewriter';
+
+	export let firstName = '';
+	export let lastName = '';
+	export let nickName = '';
+	export let ign = '';
+</script>
+
+<section id="hero" class="text-xl font-primary">
+	<header class="flex items-center min-h-screen max-w-6xl">
+		<div class="">
+			<h3 class="my-3 font-mono text-secondary text-lg md:text-xl">Hi, my name is</h3>
+			<Typewriter mode="loopOnce" keepCursorOnFinish={true} wordInterval={700}>
+				{#each [`${firstName}`, `${ign}`, `${nickName} ${lastName}`, `${nickName} ${lastName} ${firstName}`] as text}
+					<h1 class="my-3 text-5xl md:text-6xl text-left">{text}</h1>
+				{/each}
+			</Typewriter>
+			<h1 class="my-5 text-xl md:text-4xl text-left font-extrabold font-heading text-secondary">
+				NUSC w/ CS @ 2029
+			</h1>
+			<h3 class="my-2 text-lg md:text-xl">
+				💡 Conscientiously Driven | 💻 Aspiring ML Engineer | 🏞️ Outdoor Enthusiasts
+			</h3>
+		</div>
+	</header>
+	<div id="graph" class="absolute my-auto top-0 right-0 w-4/5 h-4/5 -z-10"></div>
+</section>
