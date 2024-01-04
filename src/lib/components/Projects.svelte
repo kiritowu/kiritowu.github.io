77b0a507eece04d28069eb02a4878d70b228d995
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2024-01-04 23:26:18 +0800

    Edit Project header

--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <section id="projects" class="my-10">
-	<h1 class="my-2">Project Gallery</h1>
+	<h1 class="my-2">Projects</h1>
 	<hr class="my-3 text-primary border-primary" />
 
 	{#each Object.entries(projects) as [cat, items], cat_idx}
