1ac41a5c37fe40cad4f023a9dfb86e8fab173e75
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2024-01-08 22:37:40 +0800

    Install neo4jd3-ts package & revert changes to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-		rollupOptions: {
-			// external: ['neo4jd3-ts'],
-			plugins: [nodeResolve()]
-		}
-	}
+	plugins: [sveltekit()]
 });
