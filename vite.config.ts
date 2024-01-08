42d72efbb573b78bd15439b06cbda686061bcccd
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2024-01-08 22:28:19 +0800

    Debug rollUpOption with rollup plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	build: {
 		rollupOptions: {
-			external: ['neo4jd3-ts']
+			// external: ['neo4jd3-ts'],
+			plugins: [nodeResolve()]
 		}
 	}
 });
