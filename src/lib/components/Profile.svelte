d20124930ae17cefabea2e961e7c68b52368e4ec
Wong Zhao Wu (Bryan) <70313929+kiritowu@users.noreply.github.com>
2024-01-13 16:38:59 +0800

    Enhance Profile image

--- src/lib/components/Profile.svelte
+++ src/lib/components/Profile.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { marked } from 'marked';
-	import profilePic from '$lib/images/profile-pic-cropped.jpg';
+	import profilePic from '$lib/images/profile-pic-cropped.jpg?enhanced';
 
 	export let descriptions: string[] = ['Sample description'];
 </script>
@@ -10,7 +10,11 @@
 		<h1 class="mb-6">About Me</h1>
 		<div class="flex md:flex-row flex-col justify-between">
 			<div class="md:basis-1/3 pr-10 mb-7">
-				<img class="rounded-xl shadow-lg shadow-slate-800" src={profilePic} alt="profile" />
+				<enhanced:img
+					class="rounded-xl shadow-lg shadow-slate-800"
+					src={profilePic}
+					alt="profile"
+				/>
 			</div>
 			<div class="md:basis-3/5">
 				{#each descriptions as description}
