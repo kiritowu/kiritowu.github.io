9652640de2b57861ecc667b7fc9faa20a6ddc1d6
kiritowu <wongzhaowu@gmail.com>
2024-07-27 16:20:53 +0800

    [wip] Replace Manual SVG Icon component with Simple Icon

---
+++ src/lib/components/SimpleIcon.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import * as icons from 'simple-icons';
+
+	let className = '';
+	export let baseName: keyof typeof icons;
+	export let color = 'currentColor';
+	export let size = 24;
+	export let title = '${baseName}';
+	export { className as class };
+
+	$: defaultAttrs = {
+		xmlns: 'http://www.w3.org/2000/svg',
+		width: size,
+		height: size,
+		fill: color,
+		viewBox: '0 0 24 24'
+	};
+</script>
+
+<svg {...defaultAttrs} class={className}>
+	<title>{title}</title>
+	<path d={icons[baseName].path} />
+</svg>
